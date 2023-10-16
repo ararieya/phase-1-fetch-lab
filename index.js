cff05b944303111019d92bc61f8e15ec8233c437
Ashley Rarieya <ararieya@gmail.com>
2023-10-16 08:51:03 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => console.log(json));
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error("Error fetching books:", error)); 
 }
 
 function renderBooks(books) {
